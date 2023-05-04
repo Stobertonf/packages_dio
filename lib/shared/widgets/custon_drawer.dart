ac6de6ffd29240a3eef49e5caeefaf80372e3905
stobertonf <stobertonf@gmail.com>
2023-05-04 18:17:01 -0300

    feat(Drawer): Creating Drawer.

---
+++ lib/shared/widgets/custon_drawer.dart
@@ -0,0 +1,373 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustonDrawer extends StatelessWidget {
+  const CustonDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.internetExplorer,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Abrir dio"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.mapLocationDot,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Abrir Google Maps"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.shareNodes,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Comaprtilhar"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.percent,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Indicador de porcentagem"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.batteryHalf,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Indicador da bateria"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.paperclip,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Auto Size Text"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.home,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Intl"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.flag,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("pt-br"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.flag,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Path"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.appStoreIos,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Informações pacote"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.robot,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Informações dispositivo"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.wifi,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Conexão"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.mapPin,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("GPS"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.qrcode,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("QR Code"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.camera,
+                      color: Colors.blue,
+                      size: 24,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Camera"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+        ].reversed.toList(),
+      ),
+    );
+  }
+}

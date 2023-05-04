963f200c57209ccf7eb153bd60a39917bd163b66
stobertonf <stobertonf@gmail.com>
2023-05-04 20:34:08 -0300

    feat(geolocator): Creating Geolocator Page.

---
+++ lib/geolocator/geolocator_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class GeolocatorPage extends StatefulWidget {
+  const GeolocatorPage({super.key});
+
+  @override
+  State<GeolocatorPage> createState() => _GeolocatorPageState();
+}
+
+class _GeolocatorPageState extends State<GeolocatorPage> {
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text("GPS"),
+      ),
+      body: Container(
+        child: TextButton(
+          onPressed: () async {
+            Position position = await _determinePosition();
+            print(position);
+          },
+          child: const Text("Obter posição"),
+        ),
+      ),
+    ));
+  }
+}
